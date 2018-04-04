f091bd5e9a5110541c8fc5825e264c94b424be6b
YuJin-Kim <21700140@handong.edu>
2018-04-05 04:28:59 +0900

    create package and class

---
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab07.prob2;
+
+public class Chicken {
+
+	public Chicken() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
